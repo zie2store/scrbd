4c5be504e189788a92a94ee0f529656e27ec9d54
zie2store <kuenastar101@gmail.com>
2025-06-04 21:57:56 +0700

    Update script.js

--- public/script.js
+++ public/script.js
@@ -69,3 +69,14 @@ window.onload = () => {
     });
   });
 };
+
+// Footer
+document.addEventListener("DOMContentLoaded", function () {
+  fetch('/footer.html')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('footer-container').innerHTML = data;
+    })
+    .catch(error => console.error('Error loading footer:', error));
+});
+
